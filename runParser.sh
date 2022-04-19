f6a6c8e4a88841b418120f3a594893cbcda1a432
a2v4 <avujnovich@wisc.edu>
2022-04-19 23:41:24 +0000

    make runParser.sh

---
+++ runParser.sh
@@ -0,0 +1,6 @@
+# python3 ./skeleton_parser.py ./ebay_data/items-all.json;
+# UNCOMMENT FIRST LINE BEFORE SUBMISSION
+
+# COMMENT LINES BELOW BEFORE SUBMISSION, THIS IS ONLY FOR REPO ORGANIZATION
+cd ./parsed_ebay_data;
+python3 ../skeleton_parser.py ../ebay_data/items-all.json;
